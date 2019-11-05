c48498c7c3088f282adcf3cccf4e692351b137e9
David Benson <dmbenson1978@gmail.com>
2019-11-05 14:15:49 +0000

    Reducer - restructure data
    - test passes

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -15,11 +15,29 @@ const reducer = (state = initialState, action) => {
       return { ...state, loading: true };
 
     case RECEIVE_DATA: {
-      const albums = action.results;
+      const tracks = action.results,
+            bands = {};
+
+      tracks.forEach((track) => {
+        if (!bands[track.band]) {
+          bands[track.band] = {};
+          bands[track.band].band = track.band;
+          bands[track.band].albums = {};
+        }
+
+        if (!bands[track.band].albums[track.album]) {
+          bands[track.band].albums[track.album] = {};
+          bands[track.band].albums[track.album].band = track.band;
+          bands[track.band].albums[track.album].album = track.album;
+          bands[track.band].albums[track.album].songs = [];
+        }
+
+        bands[track.band].albums[track.album].songs.push(track.song);
+      });
 
       return {
         ...state,
-        albums,
+        bands,
         loading: false,
       };
     }
