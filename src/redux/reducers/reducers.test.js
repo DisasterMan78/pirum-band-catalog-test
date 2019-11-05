7153e5b5a58bca61fa85a79e6cb5ab046c2101d7
David Benson <dmbenson1978@gmail.com>
2019-11-05 13:15:57 +0000

    Updated reducer test with correct reduced data structure
    - fails

--- src/redux/reducers/reducers.test.js
+++ src/redux/reducers/reducers.test.js
@@ -36,12 +36,60 @@ describe('DATA ACTIONS', () => {
               { band: 'Thomas J. Bergersen', album: 'Sun', song: 'Our Destiny' },
               { band: 'Nightwish', album: 'Wishmaster', song: 'Bare Grace Misery' },
               { band: 'Beatles', album: 'Abbey Road', song: 'Here Comes the Sun' },
-              { band: 'Cat Power', album: 'Sun', song: '3, 6, 9' }],
+              { band: 'Cat Power', album: 'Sun', song: '3, 6, 9' },
+              { band: 'Cat Power', album: 'Made Up Album', song: 'Fake Song' },
+            ],
           },
           expectedState = {
             ...initialState,
             loading: false,
-            albums: action.results,
+            bands: {
+              'Cat Power': {
+                band: 'Cat Power',
+                albums: {
+                  'Made Up Album': {
+                    band: 'Cat Power',
+                    album: 'Made Up Album',
+                    songs: ['Fake Song'],
+                  },
+                  Sun: {
+                    band: 'Cat Power',
+                    album: 'Sun',
+                    songs: ['Human Being', 'Always On My Own', '3, 6, 9'],
+                  },
+                },
+              },
+              Beatles: {
+                band: 'Beatles',
+                albums: {
+                  'Abbey Road': {
+                    band: 'Beatles',
+                    album: 'Abbey Road',
+                    songs: ['Maxwell\'s Silver Hammer', 'Here Comes the Sun' ],
+                  },
+                },
+              },
+              'Thomas J. Bergersen': {
+                band: 'Thomas J. Bergersen',
+                albums: {
+                  Sun: {
+                    band: 'Thomas J. Bergersen',
+                    album: 'Sun',
+                    songs: ['Our Destiny'],
+                  },
+                },
+              },
+              Nightwish: {
+                band: 'Nightwish',
+                albums: {
+                  Wishmaster: {
+                    band: 'Nightwish',
+                    album: 'Wishmaster',
+                    songs: ['Bare Grace Misery'],
+                  },
+                },
+              },
+            },
           },
           reducedData = reducer(initialState, action);
 
