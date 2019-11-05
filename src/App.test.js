13711e5ca2f008d52abfd7327c35dc4222247a08
David Benson <dmbenson1978@gmail.com>
2019-11-05 16:18:49 +0000

    App render tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
+/* globals describe, it */
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinonChai from 'sinon-chai';
+
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { initialState } from './redux/reducers';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+configure({ adapter: new Adapter() }); // configures Enzyme adapter
+
+chai.use(chaiEnzyme());
+chai.use(sinonChai);
+
+const middleware = [thunk],
+      mockStore = configureMockStore(middleware),
+      store = mockStore(initialState),
+      Component = () => (
+        <Provider store={store}>
+          <App />
+        </Provider>
+      ),
+      wrapper = mount(<Component />);
+
+describe('App rendered', () => {
+  it('should render', () => {
+    expect(wrapper.exists()).to.equal(true);
+  });
+
+  it('should render a list to hold the albums', () => {
+    expect(wrapper.find('BandList').length)
+      .to.equal(1);
+  });
 });
