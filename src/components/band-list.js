c9bc012af9499c83450c968ce4bdb99517f39995
David Benson <dmbenson1978@gmail.com>
2019-11-05 16:16:59 +0000

    Basic BandList component

---
+++ src/components/band-list.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import { useSelector } from 'react-redux';
+
+// import { fetchData } from '../redux/actions';
+// import LoadingSpinner from '../../loadingSpinner';
+
+const BandList = () => {
+  // const loading = useSelector((state) => state.loading);
+
+
+  // if (loading) {
+  //   return <LoadingSpinner />;
+  // }
+
+  return (
+    <ul>no content yet</ul>
+  );
+};
+
+export default BandList;
