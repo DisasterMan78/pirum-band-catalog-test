ff5f8675189c49c1a70249fe5a8f17f8a9f1143a
David Benson <dmbenson1978@gmail.com>
2019-11-06 13:37:06 +0000

    band-list: iterate bands as <li> elements

--- src/components/band-list.js
+++ src/components/band-list.js
@@ -4,12 +4,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from '../redux/actions/index';
 
 const BandList = () => {
+  const dispatch = useDispatch(),
+        buttonOnClick = () => {
+          dispatch(fetchData());
+        },
+        bands = useSelector((state) => state.bands);
 
   return (
     <div>
       {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-      <button type="button" id="load-songs">Show Songs</button>
-      <ul bands={bands}>no content yet</ul>
+      <button type="button" id="load-songs" onClick={buttonOnClick}>Show Songs</button>
+      <ul bands={bands}>
+        {bands && Object.keys(bands).map((bandKey) => {
+          return (<li key={bandKey}>{bandKey}</li>);
+        })}
+      </ul>
     </div>
   );
 };
