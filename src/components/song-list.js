e42e3f2d7b75e55f23d61fdf3cc94fb2bd413252
David Benson <dmbenson1978@gmail.com>
2019-11-06 14:43:04 +0000

    song-list and tests

---
+++ src/components/song-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+const SongList = (props) => {
+  const { album } = props;
+
+  return (
+    <li className="album-item">
+      <h3>{album.album}</h3>
+      <ul className="song-list">
+        {album.songs && Object.keys(album.songs).map((songKey) => {
+          return (<li className="song-item" band={album.songs[songKey] } key={songKey}>{songKey}</li>);
+        })}
+      </ul>
+    </li>
+  );
+};
+
+SongList.propTypes = {
+  album: PropTypes.shape({
+    album: PropTypes.string.isRequired,
+    band: PropTypes.string.isRequired,
+    songs: PropTypes.array.isRequired,
+  }).isRequired,
+};
+
+export default SongList;
---
+++ src/components/song-list.test.js
@@ -0,0 +1,51 @@
+/* globals describe, it */
+import React from 'react';
+
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinonChai from 'sinon-chai';
+
+import SongList from './song-list';
+import testData from './testData.json';
+
+configure({ adapter: new Adapter() }); // configures Enzyme adapter
+
+chai.use(chaiEnzyme());
+chai.use(sinonChai);
+
+const albumData = testData.bands.Beatles.albums['Abbey Road'],
+      Component = () => (<SongList album={albumData} />
+      ),
+      wrapper = mount(<Component />);
+
+describe('Song list component', () => {
+  it('should render without crashing', () => {
+    expect(wrapper.exists()).to.equal(true);
+  });
+
+  it('should render a list item to hold the songs', () => {
+    expect(wrapper.find('li.album-item').length)
+      .to.equal(1);
+  });
+
+  it('should render a heading with the album name', () => {
+    const h3 = wrapper.find('h3');
+    expect(h3.length)
+      .to.equal(1);
+
+    expect(h3.text())
+      .to.equal(albumData.album);
+  });
+
+  it('should render a list to hold the songs', () => {
+    expect(wrapper.find('ul.song-list').length)
+      .to.equal(1);
+  });
+
+  it('should render a list item for each song', () => {
+    expect(wrapper.find('li.song-item').length)
+      .to.equal(Object.keys(albumData.songs).length);
+  });
+});
