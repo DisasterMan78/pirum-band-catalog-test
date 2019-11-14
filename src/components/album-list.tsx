493833e20782e3f3293f9457a85dd6175f40e215
David Benson <dmbenson1978@gmail.com>
2019-11-14 20:01:12 +0000

    AlbumList & SongList - convert to tsx

--- src/components/album-list.tsx
+++ src/components/album-list.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { PropTypes } from 'prop-types';
 
 import ToggleButton from './toggle-button';
 import SongList from './song-list';
 
-const AlbumList = (props) => {
-  const { band } = props,
-        [open, setOpen] = useState(false),
+import { BandType } from '../prium-react-test';
+
+type AlbumProps = {
+  band: BandType;
+}
+
+type AlbumKey = any;
+
+const AlbumList = ({ band }: AlbumProps) => {
+  const [open, setOpen] = useState(false),
         toggleChildren = () => {
           // eslint-disable-next-line no-unused-expressions
           (open) ? setOpen(false) : setOpen(true);
@@ -20,19 +26,12 @@ const AlbumList = (props) => {
       <ToggleButton onClick={toggleChildren} state={open} type="albums" title={band.band} />
       <ul className="album-list">
         {/* eslint-disable-next-line arrow-body-style */}
-        {band.albums && Object.keys(band.albums).map((albumKey) => {
+        {band.albums && Object.keys(band.albums).map((albumKey: AlbumKey) => {
           return (<SongList album={band.albums[albumKey]} key={albumKey} />);
         })}
       </ul>
     </li>
   );
-};
-
-AlbumList.propTypes = {
-  band: PropTypes.shape({
-    albums: PropTypes.shape(),
-    band: PropTypes.string.isRequired,
-  }),
 };
 
 AlbumList.defaultProps = {
--- src/components/song-list.tsx
+++ src/components/song-list.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { PropTypes } from 'prop-types';
 
 import ToggleButton from './toggle-button';
+import { AlbumType } from '../prium-react-test.d';
 
-const SongList = (props) => {
-  const { album } = props,
-        [open, setOpen] = useState(false),
+type AlbumProps = {
+  album: AlbumType;
+}
+
+type SongKey = any;
+
+const SongList = ({ album }: AlbumProps) => {
+  const [open, setOpen] = useState(false),
         toggleChildren = () => {
           // eslint-disable-next-line no-unused-expressions
           (open) ? setOpen(false) : setOpen(true);
@@ -18,18 +23,10 @@ const SongList = (props) => {
       </h3>
       <ToggleButton onClick={toggleChildren} state={open} type="tracks" title={`${album.band} ${album.album}`} />
       <ul className="song-list">
-        {album.songs && Object.keys(album.songs).map((songKey) => (<li className="song-item" key={songKey}>{album.songs[songKey]}</li>))}
+        {album.songs && Object.keys(album.songs).map((songKey: SongKey) => (<li className="song-item" key={songKey}>{album.songs[songKey]}</li>))}
       </ul>
     </li>
   );
-};
-
-SongList.propTypes = {
-  album: PropTypes.shape({
-    album: PropTypes.string.isRequired,
-    band: PropTypes.string.isRequired,
-    songs: PropTypes.array.isRequired,
-  }).isRequired,
 };
 
 export default SongList;
